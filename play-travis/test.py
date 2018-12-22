bc7df726ba018b9a277a8fd92c23596a8585a768
404 found <404notfoundhard@gmain.com>
2018-12-22 23:51:59 +0700

    move test file to right directory

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,7 +3,8 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+	i = 1 + 1
+        self.assertEqual(1 + 1, i)
 
 if __name__ == '__main__':
     unittest.main()
